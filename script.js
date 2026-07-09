b09c7f7b2dd3726228d07863386c98cfb331bfa1
devprajapat001 <devprajapat.evits@gmail.com>
2026-07-09 20:53:40 +0530

    Version 2

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
 
-const messages = [
-    "Are you sure? 🥺",
-    "Sachiiii? 😭",
-    "Ek baar aur soch lo ❤️",
-    "Chocolate bhi khilaunga 🍫",
-    "Pretty Please 🥹",
-    "Free Hugs 🤗",
-    "Fine...Yes ❤️"
+const messages=[
+"Are you sure? 🥺",
+"Sachiii? 😭",
+"Ek baar aur soch lo ❤️",
+"Chocolate bhi khilaunga 🍫",
+"Pretty Please 🥹",
+"Free Hugs 🤗",
+"No is unavailable 😂",
+"Fine...Yes ❤️"
 ];
 
-let index = 0;
+let i=0;
 
-yesBtn.onclick = function(){
+yesBtn.onclick=function(){
 
-alert("Yayyyyy ❤️🥳");
-
-}
-
-noBtn.onclick = function(){
-
-if(index < messages.length){
-
-noBtn.innerHTML = messages[index];
-
-index++;
+alert("Yayyyy ❤️");
 
 }
 
+noBtn.onclick=function(){
+
+if(i<messages.length){
+
+noBtn.innerHTML=messages[i];
+
+i++;
+
 }
+
+}
+
+function createHeart(){
+
+const heart=document.createElement("div");
+
+heart.className="heart";
+
+heart.innerHTML="❤️";
+
+heart.style.left=Math.random()*100+"vw";
+
+heart.style.fontSize=(20+Math.random()*25)+"px";
+
+heart.style.animationDuration=(5+Math.random()*5)+"s";
+
+document.body.appendChild(heart);
+
+setTimeout(()=>{
+
+heart.remove();
+
+},10000);
+
+}
+
+setInterval(createHeart,300);
